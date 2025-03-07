976b3b88745377de4b7c54f44d7fda55f1bb23fe
Algus <algukenan@gmail.com>
2025-03-07 21:18:42 +0300

    First Methods

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -8,16 +8,27 @@ public class Mover : MonoBehaviour
 
     void Start()
     {
-
+        PrintInstruction();
     }
 
 
     void Update()
     {
+        MovePlayer();
+    }
+
+    void PrintInstruction()
+    {
+        Debug.Log("Welcome to the game!");
+        Debug.Log("Move using arrow keys or wasd");
+        Debug.Log("Don't bump into objects!");
+    }
+
+    void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float yValue = 0f;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue, yValue, zValue);
-
     }
 }
